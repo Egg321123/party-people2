067275e8d580e028cdcdf341ace5691e1c3df6aa
Gido <130654343+Egg321123@users.noreply.github.com>
2023-06-15 10:09:52 +0200

    Add files via upload

---
+++ Party-People-b4e1bae2-5c034a55/Person.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+public class Person
+{
+    public string? firstName { get; set; }
+    public string? lastName { get; set; }
+    public int? age { get; set; }
+    public string Introduction()
+    {
+            return $"Name: {firstName}, LastName: {lastName}, Age: {age}";
+    }
+}
---
+++ Party-People-b4e1bae2-5c034a55/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.Json;
+var jsonFile = File.ReadAllText("people.json");
+var people = JsonSerializer.Deserialize<List<Person>>(jsonFile)!;
+foreach(var item in people)
+{
+    Console.WriteLine(item.Introduction());
+}
